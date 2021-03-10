6cb4f4613b65d9582032eb3f52420a878e3d3fb2
Farnaz Jahanbakhsh <farnaz.jahanbakhsh92@gmail.com>
2021-03-10 14:39:29 -0600

    code cleanup

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'insertedApp',
@@ -22,13 +22,50 @@ export default {
       if (!authUser) {
         this.$router.push({ name: 'Login' });
       }
+      else {
+        this.fetchTitlesAndRelationships();
+      }
     });
   },
   computed: {
+    ...mapState('titles', [
+      'titles',
+      'titlesFetched'
+    ]),
+    ...mapState('relatedSources', [
+      'followedSources',
+      'trustedSources',
+    ])
+
   },
   methods: {
+    fetchTitlesAndRelationships() {
+
+      if (!this.followedSources.length)
+          this.fetchFollows();
+      if (!this.trustedSources.length)
+          this.fetchTrusteds();
+      
+      this.fetchFollowers();
+
+      if ( !this.titles.length && !this.titlesFetched ) {
+          this.setUpTitles()
+          .then( () => {
+              this.setTitlesFetched(true);
+          })
+      }
+    },
     ...mapActions('auth', [
       'getUser'
+    ]),
+    ...mapActions('titles', [
+      'setUpTitles',
+      'setTitlesFetched'
+    ]),
+    ...mapActions('relatedSources', [
+      'fetchFollows',
+      'fetchTrusteds',
+      'fetchFollowers'
     ])
   }
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,6 @@
 
 <script>
 // @ is an alias to /src
-import { mapState, mapActions } from 'vuex';
 
 export default {
   name: 'Home',
@@ -14,42 +13,12 @@ export default {
   },
   created() {
 
-    console.log('tiltes', this.titles)
-    if ( !this.titles.length && !this.titlesFetched ) {
-        this.setUpTitles()
-        .then( () => {
-            this.setTitlesFetched(true);
-        })
-    }
-    console.log('arrived in home')
-
-    if (!this.followedSources.length)
-        this.fetchFollows();
-    if (!this.trustedSources.length)
-        this.fetchTrusteds();
-    
-    this.fetchFollowers();
   },
   computed: {
-    ...mapState('titles', [
-      'titles',
-      'titlesFetched'
-    ]),
-    ...mapState('relatedSources', [
-      'followedSources',
-      'trustedSources',
-    ]),
+
   },
   methods: {
-    ...mapActions('titles', [
-      'setUpTitles',
-      'setTitlesFetched'
-    ]),
-    ...mapActions('relatedSources', [
-      'fetchFollows',
-      'fetchTrusteds',
-      'fetchFollowers'
-    ])
+
   }
 }
 </script>
