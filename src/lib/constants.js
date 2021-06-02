d5c9c18ceec2ae11150b472f5728a6dbd2e48efd
Farnaz Jahanbakhsh <farnaz.jahanbakhsh92@gmail.com>
2021-06-02 10:41:58 -0500

    update constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,8 +3,8 @@ const BASE_URL = `http://localhost:3000`;
 
 const SITE_NAME = 'Trustnet';
 const LENGTH_TO_HASH = 25;
-const MAX_TITLE_LENGTH = 160;
-const MIN_TITLE_LENGTH = 15;
+const MAX_TITLE_LENGTH = 180;
+const MIN_TITLE_LENGTH = 18;
 const FUZZY_SCORE_THRESHOLD=0.77;
 
 export default {
